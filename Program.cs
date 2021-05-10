230fa202b595a0187032a0bdea94c46df5607598
gewoih <nranenko@bk.ru>
2021-05-10 12:37:19 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string start_of_work, billing_date;
+			List<DateTime> missing_days = new List<DateTime>();
+			UInt16 N;
+
+			Console.Write("Введите дату трудоустройства: ");
+			start_of_work = Console.ReadLine();
+
+			Worker _worker = new Worker(start_of_work);
+
+			Console.Write("Введите месяц и год для рассчета компенсации за питание: ");
+			billing_date = Console.ReadLine();
+
+			Console.Write("Количество рабочих дней, пропущенных сотрудником: ");
+			N = Convert.ToUInt16(Console.ReadLine());
+			if (N > 0)
+			{
+				Console.WriteLine("Введите эти дни:");
+				for (UInt16 i = 0; i < N; i++)
+				{
+					DateTime date = DateTime.Parse(Console.ReadLine());
+					if (date.DayOfWeek != DayOfWeek.Saturday && date.DayOfWeek != DayOfWeek.Sunday)
+						missing_days.Add(date);
+				}
+			}
+
+			Console.Write("Компенсация для сотрудника составляет " + _worker.CalculateFoodMoney(DateTime.Parse(billing_date), missing_days) + "р.");
+		}
+	}
+}
---
+++ Worker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+	class Worker
+	{
+		public DateTime StartOfWork { get; } = DateTime.Now;
+
+		public Worker(string startDate)
+		{
+			this.StartOfWork = DateTime.Parse(startDate);
+		}
+		private static UInt16 CalculateBusinessDays(DateTime startDate, DateTime endDate, IEnumerable<DateTime> missingDays)
+		{
+			UInt16 calcBusinessDays = (UInt16)(1 + ((endDate - startDate).TotalDays * 5 -
+									(startDate.DayOfWeek - endDate.DayOfWeek) * 2) / 7);
+
+			if (endDate.DayOfWeek == DayOfWeek.Saturday)
+				calcBusinessDays--;
+			if (startDate.DayOfWeek == DayOfWeek.Sunday)
+				calcBusinessDays--;
+			calcBusinessDays -= (UInt16)missingDays.Count(day => day > startDate && day < endDate);
+
+			return (calcBusinessDays);
+		}
+		private (UInt16, UInt16) CalculateDays(DateTime billingDate, IEnumerable<DateTime> missingDays)
+		{
+			DateTime regularBound = new DateTime(billingDate.Year, billingDate.Month, 19);
+			DateTime increasedBound = new DateTime(billingDate.Year, billingDate.Month, DateTime.DaysInMonth(billingDate.Year, billingDate.Month));
+			UInt16 regularDays = 0, increasedDays;
+
+			if (billingDate.Month == StartOfWork.Month && billingDate.Year == StartOfWork.Year)
+				regularDays = CalculateBusinessDays(StartOfWork, regularBound, missingDays);
+			else if (billingDate.Month > StartOfWork.Month || billingDate.Year > StartOfWork.Year)
+				regularDays = CalculateBusinessDays(new DateTime(billingDate.Year, billingDate.Month, 1), regularBound, missingDays);
+			else
+				throw new Exception("В этом месяце сотрудник еще не был устроен на работу.");
+
+			increasedDays = CalculateBusinessDays(regularBound.AddDays(1), increasedBound, missingDays);
+
+			return (regularDays, increasedDays);
+		}
+		public UInt16 CalculateFoodMoney(DateTime billingDate, List<DateTime> missingDays)
+		{
+			UInt16 money, regularDays, increasedDays;
+
+			billingDate = billingDate.AddDays(DateTime.DaysInMonth(billingDate.Year, billingDate.Month) - 1);
+			(regularDays, increasedDays) = CalculateDays(billingDate, missingDays);
+
+			money = (UInt16)((regularDays * 200) + (increasedDays * 300));
+			return (money);
+		}
+	}
+}
